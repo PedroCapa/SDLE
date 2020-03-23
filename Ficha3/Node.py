b6ece1ac734e61df18925ae6a4552b63975fe9c6
lfcc98 <a81960@alunos.uminho.pt>
2020-03-23 16:03:04 +0000

    Alteração no Broadcast

--- Ficha3/Node.py
+++ Ficha3/Node.py
@@ -1,14 +1,27 @@
-#!/usr/bin/python3
-
 import networkx as nx
 import matplotlib.pyplot as plt
 import random
 
+
 class Node:
+    def handle(self, source, msg):
+        pass
+        # return novas mensagens
+
+
+class Broadcast(Node):
     def __init__(self, name, neighbords):
         self.name = name
         self.neighbords = neighbords
-    
+
     def handle(self, source, msg):
-        return (msg, self.neighbords)
-        #return novas mensagens
+        res = []
+        for neighbor in self.neighbords:
+            res.append((msg, neighbor))
+        return res
+        # return novas mensagens
+
+    def getRandomNeighbors(self):
+        if self.fanout >= len(self.neighbords):
+            return self.neighbords
+        return random.sample(self.neighbords, self.fanout)
